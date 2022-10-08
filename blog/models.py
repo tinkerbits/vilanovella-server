340dca52d31d39457c89ba1c7eeee25bb2a37bdf
tinkerbits <jon.burke.contact@gmail.com>
2022-10-08 17:52:16 +0200

    added comment section to post_detail

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,24 @@
 from django.db import models
+from django.urls import reverse
+from datetime import datetime
 
 class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now = True)
     title = models.CharField(max_length=200)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     body = models.TextField()
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now = True)
+    name = models.CharField(max_length=50)
+    comment = models.TextField()
+    related_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        commentstring = self.comment[0:100]+"..."
+        return f'{self.name}: "{commentstring}" [{(self.last_modified).strftime("%H:%M:%S, %d %b %Y")}]'
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,41 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormMixin
+from django.urls import reverse
 from .models import Post
+from .forms import CommentForm
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
-class BlogDetailView(DetailView):
+class BlogDetailView(FormMixin, DetailView):
     model = Post
     template_name = 'post_detail.html'
+    form_class = CommentForm
 
+    def get_context_data(self, **kwargs):
+        kwargs = self.kwargs
+        context = super().get_context_data(**kwargs)
+        thispost = Post.objects.get(id=kwargs.get('pk'))
+        thispostscomments = thispost.comments.all()
+        context['comments'] = thispostscomments
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        postid = self.kwargs['pk']
+        form.instance.related_post = Post.objects.get(id=postid)
+        form.save()
+        return super(BlogDetailView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk': self.object.id})
+    
 
