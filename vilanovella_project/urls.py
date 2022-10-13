32f107386de1baf627b4911e2a0e5c2d90c0580b
tinkerbits <jon.burke.contact@gmail.com>
2022-10-13 22:35:12 +0200

    fixed header menu bar using CSS Grid

--- vilanovella_project/urls.py
+++ vilanovella_project/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', include('about.urls')),
     path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
