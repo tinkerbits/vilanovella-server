54db5fbba66a6b4d0629ecfdfd72c27ccc42f32c
tinkerbits <jon.burke.contact@gmail.com>
2022-10-14 09:28:44 +0200

    modified forms.py to gain css control to comment form fields

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-name', 'placeholder': 'name'}), label='')
+    comment = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-comment', 'placeholder': 'comment'}), label='')
+
     class Meta:
         model = Comment
         fields = ('name', 'comment',)
